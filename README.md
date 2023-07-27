8493a6e6ec0ba6e6e85d947c78ad76cf4531ad57
Andrew-Rom <1811mail@gmail.com>
2023-07-27 22:30:00 +0300

    Modded README file (SQL part) and script.sql: changed type of PRIMARY KEY and FOREIGN KEY, modded relationships between tables.

--- README.md
+++ README.md
@@ -72,91 +72,82 @@ CREATE DATABASE animals;
 USE animals;
 
 CREATE TABLE human_friends (
+  id INT NOT NULL AUTO_INCREMENT,
   type_name VARCHAR(45) NOT NULL UNIQUE,
-  PRIMARY KEY (type_name)
+  PRIMARY KEY (id)
 );
 
-INSERT INTO human_friends
+INSERT INTO human_friends (type_name)
 VALUES ("pets"), ("pack_animals");
 
 CREATE TABLE pets (
+  id INT NOT NULL AUTO_INCREMENT,
+  id_type INT NOT NULL,
   group_name VARCHAR(100) NOT NULL UNIQUE,
-  type_name VARCHAR(45) NOT NULL,
-  PRIMARY KEY (group_name),
-  FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_type) REFERENCES human_friends (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-INSERT INTO pets (group_name, type_name)
-VALUES
-  ("dogs", "pets"),
-  ("cats", "pets"),
-  ("hamsters", "pets");
+INSERT INTO pets (id_type, group_name)
+VALUES 
+  (1, "dogs"), 
+  (1, "cats"), 
+  (1, "hamsters");
 
 CREATE TABLE pack_animals (
+  id INT NOT NULL AUTO_INCREMENT,
+  id_type INT NOT NULL,
   group_name VARCHAR(100) NOT NULL UNIQUE,
-  type_name VARCHAR(45) NOT NULL,
-  PRIMARY KEY (group_name),
-  FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_type) REFERENCES human_friends (id) ON DELETE CASCADE
 );
 
-INSERT INTO pack_animals (group_name, type_name)
+INSERT INTO pack_animals (id_type, group_name)
 VALUES
-  ("horses", "pack_animals"),
-  ("camels", "pack_animals"),
-  ("donkeys", "pack_animals");
+  (2, "horses"), 
+  (2, "camels"), 
+  (2, "donkeys");
 
 CREATE TABLE dogs (
-  type_name VARCHAR(45) NOT NULL,
-  group_name VARCHAR(100) NOT NULL,
   id INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (id, group_name, type_name),
-  FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (group_name) REFERENCES pets (group_name) ON DELETE CASCADE ON UPDATE CASCADE
+  id_group INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_group) REFERENCES pets (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE cats (
-  type_name VARCHAR(45) NOT NULL,
-  group_name VARCHAR(100) NOT NULL,
   id INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (id, group_name, type_name),
-  FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (group_name) REFERENCES pets (group_name) ON DELETE CASCADE ON UPDATE CASCADE
+  id_group INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_group) REFERENCES pets (id) ON DELETE CASCADE ON UPDATE CASCADE
 ); 
 
 CREATE TABLE hamsters (
-  type_name VARCHAR(45) NOT NULL,
-  group_name VARCHAR(100) NOT NULL,
   id INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (id, group_name, type_name),
-  FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (group_name) REFERENCES pets (group_name) ON DELETE CASCADE ON UPDATE CASCADE
+  id_group INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_group) REFERENCES pets (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE horses (
-  type_name VARCHAR(45) NOT NULL,
-  group_name VARCHAR(100) NOT NULL,
   id INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (id, group_name, type_name),
-  FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (group_name) REFERENCES pack_animals (group_name) ON DELETE CASCADE ON UPDATE CASCADE
+  id_group INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_group) REFERENCES pack_animals (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE camels (
-  type_name VARCHAR(45) NOT NULL,
-  group_name VARCHAR(100) NOT NULL,
   id INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (id, group_name, type_name),
-  FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (group_name) REFERENCES pack_animals (group_name) ON DELETE CASCADE ON UPDATE CASCADE
+  id_group INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_group) REFERENCES pack_animals (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE donkeys (
-  type_name VARCHAR(45) NOT NULL,
-  group_name VARCHAR(100) NOT NULL,
   id INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (id, group_name, type_name),
-  FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (group_name) REFERENCES pack_animals (group_name) ON DELETE CASCADE ON UPDATE CASCADE
+  id_group INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_group) REFERENCES pack_animals (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 ```
 
@@ -201,38 +192,38 @@ ADD command VARCHAR(100);
 - *Заполнение только низкоуровневых таблиц именами(животных), командами
 которые они выполняют и датами рождения*
 ``` SQL
-INSERT INTO dogs(animal_name, birthday, command, group_name, type_name)
+INSERT INTO dogs(animal_name, birthday, command, id_group)
 VALUES 
-  ("Chester", "2020-01-01", "run", "dogs", "pets"),
-  ("Bob", "2020-06-01", "jump", "dogs", "pets"),
-  ("Milo", "2021-01-01", "eat", "dogs", "pets");
+  ("Chester", "2020-01-01", "run", 1),
+  ("Bob", "2020-06-01", "jump", 1),
+  ("Milo", "2021-01-01", "eat", 1);
 
-INSERT INTO cats(animal_name, birthday, command, group_name, type_name)
+INSERT INTO cats(animal_name, birthday, command, id_group)
 VALUES 
-  ("Cooper", "2019-01-01", "run", "cats", "pets"),
-  ("Charlie", "2018-06-01", "play", "cats", "pets"),
-  ("Max", "2022-01-01", "sleep", "cats", "pets");
+  ("Cooper", "2019-01-01", "run", 2),
+  ("Charlie", "2018-06-01", "play", 2),
+  ("Max", "2022-01-01", "sleep", 2);
 
-INSERT INTO hamsters(animal_name, birthday, command, group_name, type_name)
+INSERT INTO hamsters(animal_name, birthday, command, id_group)
 VALUES 
-  ("Bear", "2023-01-01", "eat", "hamsters", "pets"),
-  ("MrBig", "2022-06-01", "eat", "hamsters", "pets"),
-  ("Tucker", "2022-01-01", "run", "hamsters", "pets");
+  ("Bear", "2023-01-01", "eat", 3),
+  ("MrBig", "2022-06-01", "eat", 3),
+  ("Tucker", "2022-01-01", "run", 3);
 
-INSERT INTO horses(animal_name, birthday, command, group_name, type_name)
+INSERT INTO horses(animal_name, birthday, command, id_group)
 VALUES 
-  ("April", "2018-01-01", "jump", "horses", "pack_animals"),
-  ("Apple", "2019-06-01", "run", "horses", "pack_animals");
+  ("April", "2018-01-01", "jump", 1),
+  ("Apple", "2019-06-01", "run", 1);
 
-INSERT INTO camels(animal_name, birthday, command, group_name, type_name)
+INSERT INTO camels(animal_name, birthday, command, id_group)
 VALUES 
-  ("Elliot", "2018-01-01", "go", "camels", "pack_animals"),
-  ("Iron", "2022-06-01", "stay", "camels", "pack_animals");
+  ("Elliot", "2018-01-01", "go", 2),
+  ("Iron", "2022-06-01", "stay", 2);
 
-INSERT INTO donkeys(animal_name, birthday, command, group_name, type_name)
+INSERT INTO donkeys(animal_name, birthday, command, id_group)
 VALUES 
-  ("Henry ", "2010-01-01", "do nothing", "donkeys", "pack_animals"),
-  ("Oscar", "2022-12-01", "go", "donkeys", "pack_animals");
+  ("Henry ", "2010-01-01", "do nothing", 3),
+  ("Oscar", "2022-12-01", "go", 3);
 ```
 
 <br>
@@ -245,12 +236,26 @@ DELETE FROM pack_animals
 WHERE group_name = "camels";
 ```
 
-- *Создание объединенной таблицы*
+- *Создание новой таблицы и ее заполнение в соответствии с условием задачи.
+Каждой записи присвоивается новый id 
+(из-за [особенностей](https://dev.mysql.com/doc/refman/8.0/en/innodb-auto-increment-handling.html)
+MySQL автоинкрементирование может быть с пропусками)*
 ``` SQL
-CREATE TABLE pack_animals_no_camels
-AS
-SELECT * FROM horses
-UNION SELECT * FROM donkeys;
+CREATE TABLE pack_animals_no_camels (
+  id INT AUTO_INCREMENT,
+  id_group INT NOT NULL,
+  animal_name VARCHAR(100) NOT NULL,
+  birthday DATE, 
+  command VARCHAR(100),
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_group) REFERENCES pack_animals (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO pack_animals_no_camels (id_group, animal_name, birthday, command)
+SELECT id_group, animal_name, birthday, command FROM horses;
+
+INSERT INTO pack_animals_no_camels (id_group, animal_name, birthday, command)
+SELECT id_group, animal_name, birthday, command FROM donkeys;
 ```
 
 <br>
--- db/script.sql
+++ db/script.sql
@@ -3,162 +3,170 @@ CREATE DATABASE animals;
 USE animals;
 
 CREATE TABLE human_friends (
+id INT NOT NULL AUTO_INCREMENT,
 type_name VARCHAR(45) NOT NULL UNIQUE,
-PRIMARY KEY (type_name)
+PRIMARY KEY (id)
 );
 
-INSERT INTO human_friends
+INSERT INTO human_friends (type_name)
 VALUES ("pets"), ("pack_animals");
 
 CREATE TABLE pets (
+id INT NOT NULL AUTO_INCREMENT,
+id_type INT NOT NULL,
 group_name VARCHAR(100) NOT NULL UNIQUE,
-type_name VARCHAR(45) NOT NULL,
-PRIMARY KEY (group_name),
-FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE
+PRIMARY KEY (id),
+FOREIGN KEY (id_type) REFERENCES human_friends (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-INSERT INTO pets (group_name, type_name)
-VALUES ("dogs", "pets"), ("cats", "pets"), ("hamsters", "pets");
+INSERT INTO pets (id_type, group_name)
+VALUES
+  (1, "dogs"),
+  (1, "cats"),
+  (1, "hamsters");
 
 CREATE TABLE pack_animals (
+id INT NOT NULL AUTO_INCREMENT,
+id_type INT NOT NULL,
 group_name VARCHAR(100) NOT NULL UNIQUE,
-type_name VARCHAR(45) NOT NULL,
-PRIMARY KEY (group_name),
-FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE
+PRIMARY KEY (id),
+FOREIGN KEY (id_type) REFERENCES human_friends (id) ON DELETE CASCADE
 );
 
-INSERT INTO pack_animals (group_name, type_name)
-VALUES ("horses", "pack_animals"), ("camels", "pack_animals"), ("donkeys", "pack_animals");
+INSERT INTO pack_animals (id_type, group_name)
+VALUES
+  (2, "horses"),
+  (2, "camels"),
+  (2, "donkeys");
 
 CREATE TABLE dogs (
-type_name VARCHAR(45) NOT NULL,
-group_name VARCHAR(100) NOT NULL,
 id INT NOT NULL AUTO_INCREMENT,
-PRIMARY KEY (id, group_name, type_name),
-FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY (group_name) REFERENCES pets (group_name) ON DELETE CASCADE ON UPDATE CASCADE
+id_group INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (id_group) REFERENCES pets (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE cats (
-type_name VARCHAR(45) NOT NULL,
-group_name VARCHAR(100) NOT NULL,
 id INT NOT NULL AUTO_INCREMENT,
-PRIMARY KEY (id, group_name, type_name),
-FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY (group_name) REFERENCES pets (group_name) ON DELETE CASCADE ON UPDATE CASCADE
-); 
+id_group INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (id_group) REFERENCES pets (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 CREATE TABLE hamsters (
-type_name VARCHAR(45) NOT NULL,
-group_name VARCHAR(100) NOT NULL,
 id INT NOT NULL AUTO_INCREMENT,
-PRIMARY KEY (id, group_name, type_name),
-FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY (group_name) REFERENCES pets (group_name) ON DELETE CASCADE ON UPDATE CASCADE
+id_group INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (id_group) REFERENCES pets (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE horses (
-type_name VARCHAR(45) NOT NULL,
-group_name VARCHAR(100) NOT NULL,
 id INT NOT NULL AUTO_INCREMENT,
-PRIMARY KEY (id, group_name, type_name),
-FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY (group_name) REFERENCES pack_animals (group_name) ON DELETE CASCADE ON UPDATE CASCADE
+id_group INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (id_group) REFERENCES pack_animals (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE camels (
-type_name VARCHAR(45) NOT NULL,
-group_name VARCHAR(100) NOT NULL,
 id INT NOT NULL AUTO_INCREMENT,
-PRIMARY KEY (id, group_name, type_name),
-FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY (group_name) REFERENCES pack_animals (group_name) ON DELETE CASCADE ON UPDATE CASCADE
+id_group INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (id_group) REFERENCES pack_animals (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE donkeys (
-type_name VARCHAR(45) NOT NULL,
-group_name VARCHAR(100) NOT NULL,
 id INT NOT NULL AUTO_INCREMENT,
-PRIMARY KEY (id, group_name, type_name),
-FOREIGN KEY (type_name) REFERENCES human_friends (type_name) ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY (group_name) REFERENCES pack_animals (group_name) ON DELETE CASCADE ON UPDATE CASCADE
+id_group INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (id_group) REFERENCES pack_animals (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 ALTER TABLE dogs
 ADD animal_name VARCHAR(100) NOT NULL,
-ADD birthday DATE, 
+ADD birthday DATE,
 ADD command VARCHAR(100);
 
 ALTER TABLE cats
 ADD animal_name VARCHAR(100) NOT NULL,
-ADD birthday DATE, 
+ADD birthday DATE,
 ADD command VARCHAR(100);
 
 ALTER TABLE hamsters
 ADD animal_name VARCHAR(100) NOT NULL,
-ADD birthday DATE, 
+ADD birthday DATE,
 ADD command VARCHAR(100);
 
 ALTER TABLE horses
 ADD animal_name VARCHAR(100) NOT NULL,
-ADD birthday DATE, 
+ADD birthday DATE,
 ADD command VARCHAR(100);
 
 ALTER TABLE camels
 ADD animal_name VARCHAR(100) NOT NULL,
-ADD birthday DATE, 
+ADD birthday DATE,
 ADD command VARCHAR(100);
 
 ALTER TABLE donkeys
 ADD animal_name VARCHAR(100) NOT NULL,
-ADD birthday DATE, 
+ADD birthday DATE,
 ADD command VARCHAR(100);
 
-INSERT INTO dogs(animal_name, birthday, command, group_name, type_name)
-VALUES 
-	("Chester", "2020-01-01", "run", "dogs", "pets"),
-    ("Bob", "2020-06-01", "jump", "dogs", "pets"),
-    ("Milo", "2021-01-01", "eat", "dogs", "pets");
+INSERT INTO dogs(animal_name, birthday, command, id_group)
+VALUES
+	("Chester", "2020-01-01", "run", 1),
+    ("Bob", "2020-06-01", "jump", 1),
+    ("Milo", "2021-01-01", "eat", 1);
 
-INSERT INTO cats(animal_name, birthday, command, group_name, type_name)
-VALUES 
-	("Cooper", "2019-01-01", "run", "cats", "pets"),
-    ("Charlie", "2018-06-01", "play", "cats", "pets"),
-    ("Max", "2022-01-01", "sleep", "cats", "pets");
+INSERT INTO cats(animal_name, birthday, command, id_group)
+VALUES
+	("Cooper", "2019-01-01", "run", 2),
+    ("Charlie", "2018-06-01", "play", 2),
+    ("Max", "2022-01-01", "sleep", 2);
 
-INSERT INTO hamsters(animal_name, birthday, command, group_name, type_name)
-VALUES 
-	("Bear", "2023-01-01", "eat", "hamsters", "pets"),
-    ("MrBig", "2022-06-01", "eat", "hamsters", "pets"),
-    ("Tucker", "2022-01-01", "run", "hamsters", "pets");
+INSERT INTO hamsters(animal_name, birthday, command, id_group)
+VALUES
+	("Bear", "2023-01-01", "eat", 3),
+    ("MrBig", "2022-06-01", "eat", 3),
+    ("Tucker", "2022-01-01", "run", 3);
 
-INSERT INTO horses(animal_name, birthday, command, group_name, type_name)
-VALUES 
-	("April", "2018-01-01", "jump", "horses", "pack_animals"),
-    ("Apple", "2019-06-01", "run", "horses", "pack_animals");
+INSERT INTO horses(animal_name, birthday, command, id_group)
+VALUES
+	("April", "2018-01-01", "jump", 1),
+    ("Apple", "2019-06-01", "run", 1);
 
-INSERT INTO camels(animal_name, birthday, command, group_name, type_name)
-VALUES 
-	("Elliot", "2018-01-01", "go", "camels", "pack_animals"),
-    ("Iron", "2022-06-01", "stay", "camels", "pack_animals");
+INSERT INTO camels(animal_name, birthday, command, id_group)
+VALUES
+	("Elliot", "2018-01-01", "go", 2),
+    ("Iron", "2022-06-01", "stay", 2);
 
-INSERT INTO donkeys(animal_name, birthday, command, group_name, type_name)
-VALUES 
-	("Henry ", "2010-01-01", "do nothing", "donkeys", "pack_animals"),
-    ("Oscar", "2022-12-01", "go", "donkeys", "pack_animals");
+INSERT INTO donkeys(animal_name, birthday, command, id_group)
+VALUES
+	("Henry ", "2010-01-01", "do nothing", 3),
+    ("Oscar", "2022-12-01", "go", 3);
 
 DELETE FROM pack_animals
 WHERE group_name = "camels";
 
-CREATE TABLE pack_animals_no_camels
-AS
-SELECT * FROM horses
-UNION SELECT * FROM donkeys;
+CREATE TABLE pack_animals_no_camels (
+id INT AUTO_INCREMENT,
+id_group INT NOT NULL,
+animal_name VARCHAR(100) NOT NULL,
+birthday DATE,
+command VARCHAR(100),
+PRIMARY KEY (id),
+FOREIGN KEY (id_group) REFERENCES pack_animals (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+INSERT INTO pack_animals_no_camels (id_group, animal_name, birthday, command)
+SELECT id_group, animal_name, birthday, command FROM horses;
+
+INSERT INTO pack_animals_no_camels (id_group, animal_name, birthday, command)
+SELECT id_group, animal_name, birthday, command FROM donkeys;
 
 DROP TABLE IF EXISTS animals;
 CREATE TEMPORARY TABLE animals
-AS 
-SELECT * FROM pack_animals_no_camels 
+AS
+SELECT * FROM pack_animals_no_camels
 UNION SELECT * FROM cats
 UNION SELECT * FROM hamsters
 UNION SELECT * FROM dogs;
